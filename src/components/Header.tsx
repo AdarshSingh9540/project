3b60c5d1083b02811f39b4c34199cac11d07adb6
Adarsh <adarsh9540984202@gmail.com>
2024-04-12 01:58:59 +0530

    button is added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ function Header(): JSX.Element {
           <a className="text-white mr-4" href="#">Link</a>
           <a className="text-white" href="#">Disabled</a>
         </div>
-        <form className="flex">
+        <form className="flex hidden sm:flex">
           <input className="px-2 py-1 border border-gray-600 rounded-l-md" type="search" placeholder="Search" aria-label="Search" />
           <button className="px-4 py-1 bg-gray-600 text-white rounded-r-md" type="submit">Search</button>
         </form>
--- src/components/TestPage.tsx
+++ src/components/TestPage.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
 function TestPage(): JSX.Element {
   return (
-    <div className='flex justify-between h-[85vh]'>
-      <div className='bg-black text-white w-3/12 m-8 ml-16 mr-1  rounded-2xl '>
-        left container
+    <>
+      <div className='flex justify-between flex-col sm:flex-row h-[80vh] mt-3'>
+        <div className='bg-gray-300 text-white w-full sm:w-3/12 m-8 ml-16 mr-1 mb-4 sm:mb-0 rounded-2xl'>
+          left container
+        </div>
+        <div className='bg-gray-300 text-white w-full sm:w-8/12 m-6 mr-8 rounded-2xl'>
+          Right container
+        </div>
       </div>
-      <div className='bg-black text-white w-8/12 m-6 rounded-2xl'>Right container</div>
-    </div>
+
+      <div className='flex items-center justify-end mr-10'>
+        <button className='bg-blue-600 text-white flex items-center p-2 px-6 rounded-lg'>
+          Next <HiArrowNarrowRight className='ml-2' />
+        </button>
+      </div>
+    </>
   );
 }
 
